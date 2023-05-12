90f1c5d04172572a6cbe14a12df056ac82aaf91f
Svancik <thesvancik@gmail.com>
2023-05-12 18:59:55 +0200

    who component

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -4,8 +4,87 @@ import styled from "styled-components";
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+  display: flex;
+  align-items: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  position: relative; //díky tomuto kódu se dítě (Img) vycentruje uvnitř 1. position relative rodičovského elementu => bez toho kódu by se vycentroval uprostřed obrazovky.
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 export const Who = () => {
-  return <Section>Who</Section>;
+  return (
+    <Section>
+      <Container>
+        <Left>{/* 3D model */}</Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src=" ./img/line.png" />
+            <Subtitle>Who we Are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            a creative group of designers and developers with a passion for the
+            arts
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
 };
