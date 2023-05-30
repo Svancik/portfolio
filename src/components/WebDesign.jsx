9f4d8874fe33d1f4183e85b154cc0674eb9b7413
Svancik <thesvancik@gmail.com>
2023-05-30 22:15:48 +0200

    chair.gitf import

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -1,5 +1,13 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Chair from "./Chair";
 import React from "react";
 
 export const WebDesign = () => {
-  return <div>WebDesign</div>;
+  return (
+    <Canvas>
+      <Chair />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
 };
